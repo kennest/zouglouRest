e164d5b92d3c02797c13d4f4a5a5d3195c47b899
kennest225 <kennyoulai@gmail.com>
2017-11-20 16:38:00 +0000

    First commit

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -36,11 +36,11 @@ class Event extends Model
     }
 
     public function getBeginAttribute($value){
-        return Carbon::createFromFormat('m-d-Y',$value)->toDateString();
+        return Carbon::createFromFormat('m-d-Y',$value,'Africa/Abidjan');
     }
 
     public function getEndAttribute($value){
-        return Carbon::createFromFormat('m-d-Y',$value)->toDateString();
+        return Carbon::createFromFormat('m-d-Y',$value,'Africa/Abidjan');
     }
 
 }
