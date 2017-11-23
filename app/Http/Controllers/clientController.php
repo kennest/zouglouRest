e157f6d611a74691a1cdf5c9f05eb93ca536844a
kennest225 <kennyoulai@gmail.com>
2017-11-23 10:51:50 +0000

    dropbox remove

--- app/Http/Controllers/clientController.php
+++ app/Http/Controllers/clientController.php
@@ -81,8 +81,7 @@ class clientController extends Controller
     public function SimilarEvents($word){
        $places=Place::whereHas('address' , function ($q) use($word){
                $q->where('commune','like',$word);
-       })->get();
-       $places=$places->load('events.active');
+       })->load('events')->get();
        return $places->toJson();
     }
 
