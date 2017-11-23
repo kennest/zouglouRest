e3f041d07fe7a4d156b77b949ea50d7c6de7501b
kennest225 <kennyoulai@gmail.com>
2017-11-23 11:38:55 +0000

    dropbox remove

--- resources/views/Admin/Forms/place.blade.php
+++ resources/views/Admin/Forms/place.blade.php
@@ -19,7 +19,7 @@
                 <label>Artistes:</label>
                 <select name="commune" class="form-control">
                     @foreach($communes as $c)
-                        <option value="{{$c}}" {{$r = (in_array($place->address->commune,$communes)) ? 'selected' : ''}}>{{$c}}</option>
+                        <option value="{{$c}}" {{$r = (in_array((string)$place->address->commune,$communes)) ? 'selected' : ''}}>{{$c}}</option>
                     @endforeach
                 </select>
             </div>
