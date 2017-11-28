6b40aa70591d18b1d8a87c11a00c4b6a08f36a0f
kennest225 <kennyoulai@gmail.com>
2017-11-28 17:38:45 +0000

    load similar with related

--- app/Http/Controllers/clientController.php
+++ app/Http/Controllers/clientController.php
@@ -81,7 +81,7 @@ class clientController extends Controller
     public function SimilarEvents($word){
        $places=Place::whereHas('address' , function ($q) use($word){
                $q->where('commune','like','%'.$word.'%');
-       })->get()->load(['events' => function ($query) {
+       })->get()->load(['events.artists' => function ($query) {
            $query->where('end', '>=', Carbon::now()->toDateString());
        }]);
        $places=$places->load('events.place.address');
